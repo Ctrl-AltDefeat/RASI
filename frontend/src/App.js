57a513df465fb76ab0c6f70e0f81b4a010b77d8c
orion160 <idbl64@outlook.com>
2023-10-18 21:09:08 -0500

    Format codebase

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,30 +5,30 @@ import Medicaments from "./components/Medicaments/Medicaments";
 import "bootstrap/dist/css/bootstrap.min.css"; // Agrega esta línea para importar los estilos de Bootstrap
 
 function App() {
-  return (
-    <Router>
-      <div className="container mt-4">
-        <nav>
-          <ul className="nav nav-tabs">
-            <li className="nav-item">
-              <Link to="/agendar" className="nav-link">
-                Pedir Citas Médicas
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/medicamentos" className="nav-link">
-                Medicamentos
-              </Link>
-            </li>
-          </ul>
-        </nav>
-        <Routes>
-          <Route path="/agendar" element={<Appointments />} />
-          <Route path="/medicamentos" element={<Medicaments />} />
-        </Routes>
-      </div>
-    </Router>
-  );
+    return (
+        <Router>
+            <div className="container mt-4">
+                <nav>
+                    <ul className="nav nav-tabs">
+                        <li className="nav-item">
+                            <Link to="/agendar" className="nav-link">
+                                Pedir Citas Médicas
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/medicamentos" className="nav-link">
+                                Medicamentos
+                            </Link>
+                        </li>
+                    </ul>
+                </nav>
+                <Routes>
+                    <Route path="/agendar" element={<Appointments />} />
+                    <Route path="/medicamentos" element={<Medicaments />} />
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
