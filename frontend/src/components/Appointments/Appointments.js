d517ed71f052562670c49424c5fb5a71eed110fa
lalopezpa <lalopezpa@unal.edu.co>
2023-10-20 14:12:41 -0500

    peticiones citas

--- frontend/src/components/Appointments/Appointments.js
+++ frontend/src/components/Appointments/Appointments.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-// import db from "../../data/db";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -8,72 +7,46 @@ function Appointments() {
     const [especializacion, setEspecializacion] = useState("");
     const [especializaciones, setEspecializaciones] = useState([]);
     const [doctoresEncontrados, setDoctoresEncontrados] = useState([]);
-    const [doctor, setDoctor] = useState("");
     const [dia, setDia] = useState(new Date());
     const [hora, setHora] = useState("");
+    const [resultadoBusqueda, setResultadoBusqueda] = useState(null);
 
-    // useEffect(() => {
-    //   // Llama a buscarEspecializacion cuando el componente se monta para obtener las especializaciones existentes
-    //   buscarEspecializacion();
-    // }, []); // El segundo argumento vacío [] indica que esto solo se ejecuta una vez cuando el componente se monta
+    const ip = "localhost";
 
-    // const buscarEspecializacion = () => {
-    //   const especializacionesExistentes = [
-    //     ...new Set(db.doctores.map((doc) => doc.especializacion)),
-    //   ];
+    const buscarEspecializacion = () => {
+        fetch(`http://${ip}:8000/services`)
+            .then((response) => response.json())
+            .then((data) => {
+                if (data) {
+                    const especializacionesExistentes = data.map(
+                        (item) => item.speciality
+                    );
+                    setEspecializaciones(especializacionesExistentes);
+                }
+            })
+            .catch((error) => console.error("Error fetching services:", error));
+    };
 
-    //   if (especializacionesExistentes.length > 0) {
-    //     setEspecializaciones(especializacionesExistentes);
-    //   } else {
-    //     setEspecializaciones([]);
-    //   }
-    // };
+    useEffect(() => {
+        buscarEspecializacion();
+    }, []);
 
-    // const buscarDoctoresPorEspecializacion = (especializacionSeleccionada) => {
-    //   const doctoresPorEspecializacion = db.doctores.filter(
-    //     (doc) => doc.especializacion === especializacionSeleccionada
-    //   );
-
-    //   if (doctoresPorEspecializacion.length > 0) {
-    //     setDoctoresEncontrados(doctoresPorEspecializacion);
-    //   } else {
-    //     setDoctoresEncontrados([]);
-    //   }
-    // };
-
-    // const handleEspecializacionChange = (e) => {
-    //   setEspecializacion(e.target.value);
-    //   setDoctor(""); // Restablecer el valor del doctor al cambiar la especialización
-    //   buscarDoctoresPorEspecializacion(e.target.value);
-    // };
-
-    // const solicitarCita = () => {
-    //   const nuevaCita = {
-    //     especializacion,
-    //     doctor,
-    //     dia,
-    //     hora,
-    //   };
-    //   db.citasMedicas.push(nuevaCita);
-    //   console.log("Cita solicitada:", nuevaCita);
-    // };
-
-    // const generarHoras = () => {
-    //   const horas = [];
-    //   for (let i = 0; i < 24; i++) {
-    //     for (let j = 0; j < 60; j += 15) {
-    //       const horaFormateada = `${String(i).padStart(2, "0")}:${String(
-    //         j
-    //       ).padStart(2, "0")}`;
-    //       horas.push(horaFormateada);
-    //     }
-    //   }
-    //   return horas;
-    // };
+    const buscarCitasPorEspecializacionYHora = () => {
+        fetch(
+            `http://${ip}:8000/appointments?services=${especializacion}&day=${dia}&hour=${hora}`
+        )
+            .then((response) => response.json())
+            .then((data) => {
+                setDoctoresEncontrados(data.appointments);
+            })
+            .catch((error) =>
+                console.error("Error fetching appointments:", error)
+            );
+    };
 
     return (
         <div className="container mt-4">
-            <h2 className="text-center mb-4">Mirar Citas Disponibles</h2>
+            <h2 className="text-center mb-4">Citas Disponibles</h2>
             <form>
                 <div className="row mb-3">
                     <div className="col-md-6">
@@ -81,8 +54,8 @@ function Appointments() {
                             Especialización
                         </label>
                         <select
-                            // value={especializacion}
-                            // onChange={handleEspecializacionChange}
+                            value={especializacion}
+                            onChange={(e) => setEspecializacion(e.target.value)}
                             className="form-select"
                             id="especializacion"
                         >
@@ -97,24 +70,6 @@ function Appointments() {
                             ))}
                         </select>
                     </div>
-                    {/* <div className="col-md-6">
-                        <label htmlFor="doctor" className="form-label">
-                            Doctor
-                        </label>
-                        <select
-                            value={doctor}
-                            onChange={(e) => setDoctor(e.target.value)}
-                            className="form-select"
-                            id="doctor"
-                        >
-                            <option value="">Selecciona Doctor</option>
-                            {doctoresEncontrados.map((doc) => (
-                                <option key={doc.nombre} value={doc.nombre}>
-                                    {doc.nombre}
-                                </option>
-                            ))}
-                        </select>
-                    </div> */}
                 </div>
                 <div className="row mb-3">
                     <div className="col-md-6">
@@ -129,35 +84,43 @@ function Appointments() {
                             id="dia"
                         />
                     </div>
-                    {/* <div className="col-md-6">
+                    <div className="col-md-6">
                         <label htmlFor="hora" className="form-label">
                             Hora
-                        </label> */}
-                        {/* <select
-              value={hora}
-              onChange={(e) => setHora(e.target.value)}
-              className="form-select"
-              id="hora"
-            >
-              <option value="">Selecciona Hora</option>
-              {generarHoras().map((hora) => (
-                <option key={hora} value={hora}>
-                  {hora}
-                </option>
-              ))}
-            </select> */}
-                    {/* </div> */}
+                        </label>
+                        <input
+                            type="text"
+                            value={hora}
+                            onChange={(e) => setHora(e.target.value)}
+                            className="form-control"
+                            id="hora"
+                            placeholder="Ingrese la hora"
+                        />
+                    </div>
                 </div>
                 <div className="text-center">
                     <button
                         type="button"
-                        // onClick={solicitarCita}
+                        onClick={buscarCitasPorEspecializacionYHora}
                         className="btn btn-primary"
                     >
-                        Solicitar Cita
+                        Buscar Citas
                     </button>
                 </div>
             </form>
+            {/* Mostrar resultados encontrados */}
+            {doctoresEncontrados.length > 0 && (
+                <div className="mt-4">
+                    <h3>Resultados Encontrados:</h3>
+                    <ul>
+                        {doctoresEncontrados.map((cita) => (
+                            <li key={cita.id}>
+                                {cita.doctor} - {cita.fecha} - {cita.hora}
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            )}
         </div>
     );
 }
