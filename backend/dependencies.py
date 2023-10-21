c4aed6f9b394bd67db032eba95eeae4f39b5c49d
Isaac David <61389980+orion160@users.noreply.github.com>
2023-10-21 07:02:11 +0000

    Change deps

--- backend/dependencies.py
+++ backend/dependencies.py
@@ -2,10 +2,16 @@ from typing import Annotated
 from fastapi import Depends
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 
+db_user = "admin"
+db_pass = "password"
+db_host = "postgres"
+db_port = 5432
+db_name = "rasi"
+
 
 async def get_db_session_factory() -> async_sessionmaker:
     engine = create_async_engine(
-        "postgresql+psycopg://<db_user>:<db_pass>@<db_host>:<db_port>/<db_name>",
+        f"postgresql+psycopg://{db_user}:{db_pass}@{db_host}:{db_port}/{db_name}",
         echo=True,
     )
     return async_sessionmaker(engine)
