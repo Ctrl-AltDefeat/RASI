d30c7626f2687fc32c96976e0f0d7a98fee511ec
Isaac David <61389980+orion160@users.noreply.github.com>
2023-10-21 08:02:22 +0000

    Completed Alembic config

--- backend/dependencies.py
+++ backend/dependencies.py
@@ -2,16 +2,10 @@ from typing import Annotated
 from fastapi import Depends
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 
-db_user = "admin"
-db_pass = "password"
-db_host = "postgres"
-db_port = 5432
-db_name = "rasi"
-
 
 async def get_db_session_factory() -> async_sessionmaker:
     engine = create_async_engine(
-        f"postgresql+psycopg://{db_user}:{db_pass}@{db_host}:{db_port}/{db_name}",
+        "postgresql+psycopg://admin:password@postgres:5432/rasi",
         echo=True,
     )
     return async_sessionmaker(engine)
--- backend/medicaments/entity.py
+++ backend/medicaments/entity.py
@@ -10,18 +10,17 @@ class MedicamentEntity(BaseEntity):
     id: Mapped[int] = mapped_column(primary_key=True, init=False)
     name: Mapped[str]
     brand: Mapped[str]
-    units_available: Mapped[int]
 
 
 class MedicamentAvailabilityEntity(BaseEntity):
     __tablename__ = "medicament_availability"
 
-    medicament_id: Mapped[int] = mapped_column(
-        ForeignKey("medicament.id"), primary_key=True, init=False
-    )
-    medicament: Mapped[MedicamentEntity] = relationship()
     ips_id: Mapped[int] = mapped_column(
         ForeignKey("ips.id"), primary_key=True, init=False
     )
     ips: Mapped[IPSEntity] = relationship()
+    medicament_id: Mapped[int] = mapped_column(
+        ForeignKey("medicament.id"), primary_key=True, init=False
+    )
+    medicament: Mapped[MedicamentEntity] = relationship()
     units_available: Mapped[int]
